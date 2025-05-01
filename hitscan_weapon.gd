c3cd7efafb2a259dfe058185ea06de8210298477
Samar <samarjomysujith606@gmail.com>
2025-05-01 22:30:12 +0530

    Added cooldown for weapons. Will introduce recoil

---
+++ hitscan_weapon.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@onready var timer:Timer=$Timer
+
+@export var fire_rate:=14.0
+@export var recoil:=0.05
+@export var weapon_mesh:Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.start(1.0/fire_rate) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if timer.is_stopped():
+			shoot()
+
+
+func shoot() -> void:
+	timer.start(1.0/fire_rate)
+	print("weapon fired")
